27550a8793df52c96cd7bddd8148a61c835e797a
Chunsang Jeong <chunsangjeong@gmail.com>
2019-06-09 18:09:53 +0900

    Add feature to create price data by using stock code
    Signed-off-by: Chunsang Jeong <chunsangjeong@gmail.com>

--- scrapping.py
+++ scrapping.py
@@ -2,20 +2,23 @@ import pandas as pd
 import datetime as dt
 import matplotlib.pyplot as plt
 import csv
-import urllib.request, json
+import urllib.request
+import json
 import os
 import numpy as np
 import tensorflow as tf
+import re
 
 SAVED_CSV_FILE = 'data/price.csv'
 STOCK_LIST_FILE = 'data/stock_list.csv'
+
 
 def read_stock_price_page(stock_code, page_num):
     '''
     네이버 주식시세 페이지에 접속하여 table을 dataframe으로 가져와서 정리
     '''
     target_url = ('http://finance.naver.com/item/sise_day.nhn?code='+ stock_code + '&page=' + str(page_num))
-    print('[INFO] target url: {}'.format(target_url) )
+    #print('[INFO] target url: {}'.format(target_url))
     data = pd.read_html(target_url)
     data = data[0]
     data.columns = ['Date', 'Close', 'Diff', 'Open', 'High', 'Low', 'Volume']
@@ -26,14 +29,15 @@ def read_stock_price_page(stock_code, page_num):
     price_data['Date'] = pd.to_datetime(price_data['Date'], format='%Y/%m/%d')
     return price_data
 
+
 def stock_price_pages_to_df(code, days_limit=30):
     '''
-    오늘부터 days_limit 일수 만큼 이전 날짜 주가를 가져온다.
+    Pull and add from today to the number of days_limit.
     '''
-
+    print('---code:{}'.format(code))
     df_list_price = []
     page = 1
-    while True:
+    while True:  # need to change the code later
         try: 
             data = read_stock_price_page(code, page)
             time_limit = dt.datetime.now() - data['Date'][0]
@@ -43,7 +47,8 @@ def stock_price_pages_to_df(code, days_limit=30):
             df_list_price.append(data)
             page = page + 1
 
-        except: break
+        except:
+            break
     df_price = pd.concat(df_list_price)
 
     df_price = df_price.reset_index(drop=True)
@@ -51,39 +56,75 @@ def stock_price_pages_to_df(code, days_limit=30):
     df_price = df_price.drop('Diff', axis=1)
     df_price = df_price.sort_values('Date')
 
-    df_price.to_csv(SAVED_CSV_FILE)
+    filename = get_data_filename(code)
+    df_price.to_csv(filename,index=False)
+    print('[INFO] Saved file as {}'.format(filename))
     return df_price
 
-def show_graph(filename = SAVED_CSV_FILE):
+
+def show_graph(filename=SAVED_CSV_FILE):
     try:
         df = pd.read_csv(filename, na_values=["", " ", "-"])
+        df = df.reset_index()
+        f=df.drop(['index'],axis=1)
     except FileNotFoundError:
         print("File Not Found : " + filename)
         return
 
     # show as graph
-    plt.figure(figsize = (12,6))
+    plt.figure(figsize=(12, 6))
     plt.plot(range(df.shape[0]),(df['Low']+df['High'])/2.0)
     plt.xticks(range(0,df.shape[0],30),df['Date'].loc[::30],rotation=45)
     plt.xlabel('Date',fontsize=18)
     plt.ylabel('Mid Price',fontsize=18)
     plt.show()
 
-def find_stock_index(filename = STOCK_LIST_FILE):
+
+def find_stock_index(company_name='삼성'):
     try:
-        df = pd.read_csv(filename, na_values=["", " ", "-"])
+        df = pd.read_csv(STOCK_LIST_FILE, na_values=["", " ", "-"])
+        df = df.reset_index()
+        f=df.drop(['index'],axis=1)
     except FileNotFoundError:
         print("File Not Found : " + filename)
         return
 
-    index = '005300'
-    print(df)
+    stock_list = df[['종목코드','기업명']]
+    #print(stock_list)
+
+    found_list = stock_list[stock_list['기업명'].str.contains(company_name, na=False)]
+    print('[INFO]found_list:{}'.format(found_list))
+    ## add error handling code when finding more than one list --> just return the first result
+    code = found_list['종목코드']
+
+    print('[INFO]Processing for the companies:\n {}'.format(found_list))
+    stock_code=str(list(found_list['종목코드']))
+    stock_code=re.sub("\'|\[|\]", "", stock_code).rjust(6,'0')
+    print('[INFO]stock_code:{}'.format(stock_code))
+
+    return stock_code
+
+def get_data_filename(stock_code):
+    saved_dir = 'data'
+    filename = 'price_'
+    filename = saved_dir+'/'+filename+stock_code+'.csv'
+    print('[INFO]filename:{}'.format(filename))
+    return filename
+
+
+def generate_report(company_name='삼성'):
+    stock_code = find_stock_index(company_name)
+    filename = get_data_filename(stock_code)
+    stock_price_pages_to_df(stock_code, 100)
+    #show_graph(filename)
+
 
 # Test to get & dave data
-
+'''
 stock_code = '005380'
 days_limit = 500
 df = stock_price_pages_to_df(stock_code, days_limit)
 print(df)
+'''
 
-#show_graph()
+generate_report('삼성화재')
